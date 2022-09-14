9495a2690a88f62a1606dd31a4391230ab148a0b
TrofimovV <xenus63@yandex.ru>
2022-09-14 08:40:00 +0400

    add docker-compose

--- internal/user/store.go
+++ internal/user/store.go
@@ -23,16 +23,18 @@ type Task struct {
 }
 
 func NewConnectDB(logger *logrus.Entry, cfg *configs.ConfigDatabase) (*sql.DB, error) {
-	dataConfig := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable",
-		cfg.Host, cfg.Username, cfg.Password, cfg.DBName)
+	//dataConfig := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable",
+	dataConfig := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
 	//logging with "pkg/logging"
 	logger.Debugf("POSTGRES_DB = %s POSTGRES_PASSWORD = %s", cfg.DBName, cfg.Password)
+
 	db, err := sql.Open("postgres", dataConfig)
 	if err != nil {
 		return nil, err
 	}
 	if err := db.Ping(); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("db.Ping error : %v", err)
 	}
 	return db, nil
 }
